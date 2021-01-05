4336f895ad7ebad5e5e8c4313f5dda1557759ddb
David Sanchez <davidmsanchez250@gmail.com>
2021-01-05 17:04:23 -0500

    added character length prompt. Added formula for how to find random string in an array using math random. Added comments.

--- script.js
+++ script.js
@@ -1,27 +1,39 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var lowercaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-var numberArray = ["1","2","3","4","5","6","7","8","9","0"]
-var specialCharacterArray = ["!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","}","[","]",",",".","<",">","/","?",";",":","'"]
 
+
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var numbers = ["1","2","3","4","5","6","7","8","9","0"]
+var specialCharacters = ["!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","}","[","]",",",".","<",">","/","?",";",":","'"]
+
+// Example of how to use Math random to get random char in array
+console.log(specialCharacters[Math.floor(Math.random()*specialCharacters.length)]);
+
+//Maybe use charAt() method to find random character in an index instead of individualizing each letter in array? example: it would find random character in this string [abcdefghijklmnop]
 
 // Write password to the #password input
 function writePassword() {
-  var uppercase = confirm("Would you like to add any uppercase?");
-    if (uppercase){
+
+  // Would I need to use parseint() before prompt?
+  var characterLength = prompt("How many characters would you like in your password?");
+
+// Create for-loop based on number of characters from prompt? Then include upperacse,lowercase,numbers, and specialchars in loop using + ?
+
+  var uppercaseConf = confirm("Would you like to add any uppercase?");
+    if (uppercaseConf){
 
     }
-  var lowercase = confirm("Would you like to add any lowercase?");
-    if (lowercase){
+  var lowercaseConf = confirm("Would you like to add any lowercase?");
+    if (lowercaseConf){
 
     }
-  var numbers = confirm("Would you like to add any numbers?");
-    if (numbers){
+  var numbersConf = confirm("Would you like to add any numbers?");
+    if (numbersConf){
 
     }
-  var specialCharacters = confirm("Would you like to add any special characters?");
-    if (specialCharacters){
+  var specialCharactersConf = confirm("Would you like to add any special characters?");
+    if (specialCharactersConf){
 
     }
 
